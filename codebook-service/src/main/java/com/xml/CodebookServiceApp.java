6daa48f4e5e597bad688a3a31e5d479123a1fd25
igorMales98 <igor_malesevic@yahoo.com>
2020-05-13 21:19:01 +0200

    Created all microservices and eureka server and clients.

--- codebook-service/src/main/java/com/xml/CodebookServiceApp.java
+++ codebook-service/src/main/java/com/xml/CodebookServiceApp.java
@@ -3,9 +3,11 @@ package com.xml;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CodebookServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(CodebookServiceApp.class, args);
