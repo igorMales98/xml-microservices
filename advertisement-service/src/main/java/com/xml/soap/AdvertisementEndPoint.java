c2c4b244d640e314e1c28b4d3264a32d2012d881
Stefan <39972111+stefansumar@users.noreply.github.com>
2020-06-18 22:27:36 +0200

    SOAP

--- advertisement-service/src/main/java/com/xml/soap/AdvertisementEndPoint.java
+++ advertisement-service/src/main/java/com/xml/soap/AdvertisementEndPoint.java
@@ -1,13 +1,19 @@
 package com.xml.soap;
 
+import com.xml.dto.CreateAdvertisementDto;
+import com.xml.model.Advertisement;
+import com.xml.model.Car;
 import com.xml.service.AdvertisementService;
 import localhost._8085.advertisement_service_schema.AdvertisementRequest;
 import localhost._8085.advertisement_service_schema.AdvertisementResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import java.time.LocalDateTime;
 
 
 @Endpoint
@@ -20,8 +26,20 @@ public class AdvertisementEndPoint {
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "advertisementRequest")
     @ResponsePayload
     public AdvertisementResponse createAdvertisement(@RequestPayload AdvertisementRequest request) {
-        System.out.println("UOPAAAAAAAAAA :O");
+        System.out.println("SOAP - Create Advertisement");
         AdvertisementResponse response = new AdvertisementResponse();
+        CreateAdvertisementDto advertisementDto = new CreateAdvertisementDto();
+
+        advertisementDto.setAvailableFrom(LocalDateTime.parse(request.getAvailableFrom()));
+        advertisementDto.setAvailableTo(LocalDateTime.parse(request.getAvailableTo()));
+        advertisementDto.setAdvertiserId(request.getAdvertiserId());
+        advertisementDto.setAllowedDistance(request.getCar().getAllowedDistance());
+        advertisementDto.setHasACDW(request.getCar().isCollisionDamageWaiverExists());
+
+
+
+
+
 
         return response;
     }
